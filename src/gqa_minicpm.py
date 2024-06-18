d0bde7b94c15408b120d2adf45adad4d2b7f7965
zli <zli@mail.com>
2024-06-18 13:16:49 +0200

    minicpm test update

--- src/gqa_minicpm.py
+++ src/gqa_minicpm.py
@@ -5,10 +5,10 @@ from PIL import Image
 from transformers import AutoModel, AutoTokenizer
 import json
 import os
+import os.path as osp
 from tqdm import tqdm
-from pathlib import Path
 
-base_path = Path(__file__).parent.parent
+base_dir = 'data/GQA'
 
 # Set the seed
 seed_value = 42
@@ -22,17 +22,17 @@ torch.cuda.manual_seed_all(seed_value)  # if using multi-GPU
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-model = AutoModel.from_pretrained('openbmb/MiniCPM-Llama3-V-2_5-int4', trust_remote_code=True)
-tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-Llama3-V-2_5-int4', trust_remote_code=True)
+model = AutoModel.from_pretrained('weights/hf/MiniCPM-Llama3-V-2_5-int4', trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained('weights/hf/MiniCPM-Llama3-V-2_5-int4', trust_remote_code=True)
 model.eval()
 
 # Load test-dev dataset
-with open(base_path/'data'/'questions'/'testdev_balanced_questions.json') as f:
+with open(osp.join(base_dir, 'questions/testdev_balanced_questions.json')) as f:
     data_questions = json.load(f)
 
 # get the answer from the model for the given question and image
 def get_answer(question, image_id):
-    image_path = os.path.join(base_path/'data'/'images', image_id + '.jpg')
+    image_path = os.path.join(base_dir, 'images', image_id + '.jpg')
     image = Image.open(image_path).convert('RGB')
     msgs = [{'role': 'user', 'content': question}]
     res = model.chat(
@@ -62,7 +62,7 @@ def get_predictions(data_keys):
             question_id = data
             instruction = 'Provide a short answer to the question and always start the answer with yes or no:'
             prediction = get_answer(instruction + question, image_id)
-            
+
             # save predictions as [{"questionId": str, "prediction": str}]
             predictions['questionId'] = question_id
             predictions['prediction'] = prediction
@@ -70,10 +70,10 @@ def get_predictions(data_keys):
             image_ids_list.append(image_id)
             answers_list.append(data_questions[data]['answer'])
             questions_list.append(question)
-    
+
     return predictions_list, image_ids_list, answers_list, questions_list
 
-# compute the accuracy 
+# compute the accuracy
 def evaluate(predictions_list, answers_list):
     # get the first word of each prediction
     predictions_short = [predictions_list[i]['prediction'].split(' ')[0] for i, _ in enumerate(predictions_list)]
@@ -102,11 +102,12 @@ if __name__ == '__main__':
     accuracy, idx_wrong = evaluate(predictions_list, answers_list)
     print('Accuracy: ', accuracy)
 
-    # save all predictions as json 
-    with open(base_path/'test_results'/'predictions_testdev_balanced.json', 'w') as f:
-        json.dump(predictions_list, f, indent=4)
+    # save all predictions as json
+    os.makedirs('test_results', exist_ok=True)
+    with open('test_results/predictions_testdev_balanced.json', 'w') as f:
+        json.dump(predictions_list, f, indent=2)
 
     # save the wrong predictions as json
-    with open(base_path/'test_results'/'wrong_predictions_testdev_balanced.json', 'w') as f:
+    with open('test_results/wrong_predictions_testdev_balanced.json', 'w') as f:
         wrong_predictions = [{'image_id': image_ids_list[i], 'question': questions_list[i], 'prediction': predictions_list[i]['prediction'], 'answer': answers_list[i]} for i in idx_wrong]
-        json.dump(wrong_predictions, f, indent=4)
+        json.dump(wrong_predictions, f, indent=2)
